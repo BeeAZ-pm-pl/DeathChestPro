1ba94903fb8c82450db3ec0844463ccfa8c7a936
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2022-09-20 17:32:45 +0700

    Document Reformatted

--- src/BeeAZ/DeathChestPro/DeathChestPro.php
+++ src/BeeAZ/DeathChestPro/DeathChestPro.php
@@ -9,36 +9,36 @@ use pocketmine\world\Position;
 use pocketmine\block\VanillaBlocks;
 use pocketmine\block\tile\Chest;
 
-class DeathChestPro extends PluginBase implements Listener{
-  
-  public function onEnable(): void{
-   $this->getServer()->getPluginManager()->registerEvents($this, $this);
-   $this->saveDefaultConfig();
-  }
-  
-  public function onDeath(PlayerDeathEvent $event){
-   $player = $event->getPlayer();
-   $cfg = $this->getConfig()->getAll();
-   $x = (int) $player->getPosition()->getX();
-   $y = (int) $player->getPosition()->getY();
-   $z = (int) $player->getPosition()->getZ();
-   $world = $player->getWorld();
-   $this->getServer()->broadcastMessage(str_replace(["{x}", "{y}", "{z}", "{world}", "{player}"], [$x, $y, $z, $world->getFolderName(), $player->getName()], $cfg["message"]));
-   $world->setBlock(new Position($x, $y, $z, $world), VanillaBlocks::CHEST());
-   $world->setBlock(new Position($x + 1, $y, $z, $world), VanillaBlocks::CHEST());
-   $tile = $world->getTile(new Position($x, $y, $z, $world));
-   $tile2 = $world->getTile(new Position($x + 1, $y, $z, $world));
-     if($tile instanceof Chest && $tile2 instanceof Chest){
-       $tile->pairWith($tile2);
-       $tile2->pairWith($tile);
-         foreach($player->getInventory()->getContents() as $item){
-           $tile->getInventory()->addItem($item);
-           $player->getInventory()->clearAll();
-         }
-         foreach($player->getArmorInventory()->getContents() as $item){
-           $tile->getInventory()->addItem($item);
-           $player->getArmorInventory()->clearAll();
-        }
-      }
+class DeathChestPro extends PluginBase implements Listener {
+
+    public function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveDefaultConfig();
     }
-  }
+
+    public function onDeath(PlayerDeathEvent $event) {
+        $player = $event->getPlayer();
+        $cfg = $this->getConfig()->getAll();
+        $x = (int) $player->getPosition()->getX();
+        $y = (int) $player->getPosition()->getY();
+        $z = (int) $player->getPosition()->getZ();
+        $world = $player->getWorld();
+        $this->getServer()->broadcastMessage(str_replace(["{x}", "{y}", "{z}", "{world}", "{player}"], [$x, $y, $z, $world->getFolderName(), $player->getName()], $cfg["message"]));
+        $world->setBlock(new Position($x, $y, $z, $world), VanillaBlocks::CHEST());
+        $world->setBlock(new Position($x + 1, $y, $z, $world), VanillaBlocks::CHEST());
+        $tile = $world->getTile(new Position($x, $y, $z, $world));
+        $tile2 = $world->getTile(new Position($x + 1, $y, $z, $world));
+        if ($tile instanceof Chest && $tile2 instanceof Chest) {
+            $tile->pairWith($tile2);
+            $tile2->pairWith($tile);
+            foreach ($player->getInventory()->getContents() as $item) {
+                $tile->getInventory()->addItem($item);
+                $player->getInventory()->clearAll();
+            }
+            foreach ($player->getArmorInventory()->getContents() as $item) {
+                $tile->getInventory()->addItem($item);
+                $player->getArmorInventory()->clearAll();
+            }
+        }
+    }
+}
